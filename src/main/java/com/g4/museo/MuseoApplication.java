462c4772053500fe9ab428f0fd632741930bda30
Prenat <prenatthibault@gmail.com>
2021-04-28 15:30:43 +0200

    Creation branche pour l'entête de la page principal

--- src/main/java/com/g4/museo/MuseoApplication.java
+++ src/main/java/com/g4/museo/MuseoApplication.java
@@ -2,7 +2,6 @@ package com.g4.museo;
 
 import com.g4.museo.ui.GuiApplication;
 import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
