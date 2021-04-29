195c2222e4d0353b9bd3dc1f59c3ebb46cb8c7e0
Prenat <prenatthibault@gmail.com>
2021-04-29 10:18:16 +0200

    ActionButton_management

--- src/main/java/com/g4/museo/ui/fxml/MainFxmlController.java
+++ src/main/java/com/g4/museo/ui/fxml/MainFxmlController.java
@@ -5,11 +5,16 @@ import javafx.beans.Observable;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.InputStream;
 import java.util.List;
@@ -25,12 +30,5 @@ public class MainFxmlController {
         TableColumn<ArtworkDTO, String> artist = new TableColumn<>("Nom de l'artiste");
         artist.setCellValueFactory(c-> new SimpleStringProperty("Test"));
         artworkGrid.getColumns().addAll(name, artist);
-    }
-
-    public void ButtonAdministation(){
-
-        Button button2 = new Button("Button with Text & Image");
-        button2.setGraphic(new ImageView());
-
     }
 }
